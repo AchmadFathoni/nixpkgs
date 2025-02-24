d25f4868dd31893a8f6a030000b92537d7c24672
Achmad Fathoni <fathoni.id@gmail.com>
2025-02-24 10:15:31 +0700

    cloudflared: 2024.10.0 -> 2025.2.0

--- pkgs/applications/networking/cloudflared/default.nix
+++ pkgs/applications/networking/cloudflared/default.nix
@@ -8,13 +8,13 @@
 
 buildGoModule rec {
   pname = "cloudflared";
-  version = "2024.10.0";
+  version = "2025.2.0";
 
   src = fetchFromGitHub {
     owner = "cloudflare";
     repo = "cloudflared";
     tag = version;
-    hash = "sha256-xCLLWe15+YmU3SyWkclzHBojHi32nUJGe4xY3NZC05M=";
+    hash = "sha256-K1xMk9y9cxqbLqXC0TU62kSqq1WxTF8T/fAgE0THOLQ=";
   };
 
   vendorHash = null;
@@ -86,3 +86,4 @@ buildGoModule rec {
     mainProgram = "cloudflared";
   };
 }
+
