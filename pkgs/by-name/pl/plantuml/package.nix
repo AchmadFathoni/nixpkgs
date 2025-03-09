e66b335f17b5992741f4217ec91102b7569c1ba3
Achmad Fathoni <fathoni.id@gmail.com>
2025-03-10 01:35:42 +0700

    plantuml: 1.2024.7 -> 1.2025.2

--- pkgs/by-name/pl/plantuml/package.nix
+++ pkgs/by-name/pl/plantuml/package.nix
@@ -10,11 +10,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "plantuml";
-  version = "1.2024.7";
+  version = "1.2025.2";
 
   src = fetchurl {
     url = "https://github.com/plantuml/plantuml/releases/download/v${finalAttrs.version}/plantuml-pdf-${finalAttrs.version}.jar";
-    hash = "sha256-8BlAPc2BeojyTcZVboUA6+sOA5AO5eG871mSIWinm5E=";
+    hash = "sha256-mDP72BLe7+WqjCsxIrhF//jVQVpmU0jwgFeH2sW1mH0=";
   };
 
   nativeBuildInputs = [
