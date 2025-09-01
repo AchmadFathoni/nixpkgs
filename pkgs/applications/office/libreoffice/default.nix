af68510d5c966a7afd4b6d57d67aef3a9abe98f2
Tom van Dijk <18gatenmaker6@gmail.com>
2025-09-01 19:58:28 +0200

    libreoffice-fresh: 25.2.3.2 -> 25.8.1.1, the manual parts

--- pkgs/applications/office/libreoffice/default.nix
+++ pkgs/applications/office/libreoffice/default.nix
@@ -35,7 +35,6 @@
   cairo,
   which,
   icu,
-  boost,
   jdk21,
   ant,
   cups,
@@ -71,7 +70,6 @@
   libcdr,
   lcms2,
   unixODBC,
-  mdds,
   sane-backends,
   mythes,
   libexttextcat,
@@ -99,7 +97,6 @@
   abseil-cpp,
   libepubgen,
   libetonyek,
-  liborcus,
   libpng,
   langs ? [
     "ar"
@@ -342,8 +339,9 @@ stdenv.mkDerivation (finalAttrs: {
     # Revert part of https://github.com/LibreOffice/core/commit/6f60670877208612b5ea320b3677480ef6508abb that broke zlib linking
     ./readd-explicit-zlib-link.patch
 
+  ]
+  ++ lib.optionals (lib.versionOlder version "25.8") [
     # Backport patch to fix build with Poppler 25.05
-    # FIXME: conditionalize/remove as upstream updates
     (fetchpatch2 {
       url = "https://github.com/LibreOffice/core/commit/0ee2636304ac049f21415c67e92040f7d6c14d35.patch";
       includes = [ "sdext/*" ];
@@ -401,7 +399,6 @@ stdenv.mkDerivation (finalAttrs: {
       abseil-cpp
       ant
       bluez5
-      boost
       box2d_2
       cairo
       clucene_core_2
@@ -454,7 +451,6 @@ stdenv.mkDerivation (finalAttrs: {
       libmwaw
       libmysqlclient
       libodfgen
-      liborcus
       xorg.libpthreadstubs
       librdf_redland
       librevenge
@@ -470,7 +466,6 @@ stdenv.mkDerivation (finalAttrs: {
       libzmf
       libwebp
       lp_solve
-      mdds
       mythes
       ncurses
       neon
@@ -539,8 +534,6 @@ stdenv.mkDerivation (finalAttrs: {
     "--without-buildconfig-recorded"
 
     (lib.withFeature withHelp "help")
-    "--with-boost=${getDev boost}"
-    "--with-boost-libdir=${getLib boost}/lib"
     "--with-beanshell-jar=${bsh}"
     "--with-vendor=NixOS"
     "--disable-report-builder"
@@ -580,7 +573,6 @@ stdenv.mkDerivation (finalAttrs: {
     "--with-system-libs"
     "--with-system-libwps"
     "--with-system-lpsolve"
-    "--with-system-mdds"
     "--with-system-openldap"
     "--with-system-openssl"
     "--with-system-orcus"
@@ -590,6 +582,7 @@ stdenv.mkDerivation (finalAttrs: {
     # TODO: package these as system libraries
     "--without-system-altlinuxhyph"
     "--without-system-frozen"
+    "--without-system-libeot"
     "--without-system-libfreehand"
     "--without-system-libmspub"
     "--without-system-libnumbertext"
@@ -599,6 +592,12 @@ stdenv.mkDerivation (finalAttrs: {
     "--without-system-dragonbox"
     "--without-system-libfixmath"
 
+    # TODO: bump this to 0.20
+    "--without-system-orcus"
+
+    # TODO: bump this to 3.0 (#382851)
+    "--without-system-mdds"
+
     # requires an oddly specific, old version
     "--without-system-hsqldb"
 
@@ -607,6 +606,9 @@ stdenv.mkDerivation (finalAttrs: {
 
     # is packaged but headers can't be found because there is no pkg-config file
     "--without-system-zxcvbn"
+
+    # cannot find headers, no idea why
+    "--without-system-boost"
   ]
   ++ optionals kdeIntegration [
     "--enable-kf${qtMajor}"
